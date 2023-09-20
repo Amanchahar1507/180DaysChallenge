117fea958bce8d67cb6b670f34ea7ead1ec5a391
Amanchahar1507 <103381910+Amanchahar1507@users.noreply.github.com>
2023-09-20 19:30:06 +0530

    Add files via upload

---
+++ Day3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    cout<<"6*3";
+}
+
+
+//PRODUCT TO NUMBER
+
+// int main(){
+//     cout<<"enter two number";
+//     int a,b;
+//     cin>>a;
+//     cin>>b;
+
+//     int pro = a*b;
+//     cout<<pro<<endl;
+    
+// }
+
+//SUBTRACT TWO NUMBER
+
+// int main(){
+//     int a,b;
+//     cin>>a;
+//     cin>>b;
+
+//     int sub = abs(a-b);
+//     cout<<sub<<endl;
+// }
