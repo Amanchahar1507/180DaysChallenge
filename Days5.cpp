4590df396da71faf411c806533aa806fcb96e39e
Amanchahar1507 <103381910+Amanchahar1507@users.noreply.github.com>
2023-09-22 19:16:30 +0530

    Add files via upload

---
+++ Days5.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // 1. PRINT A NUMBER 280 TO 250
+
+    // int n = 280;
+    // for(int i=n;i>=250;i--){
+    //     cout<<i<<endl;
+    // }
+
+    // 2. PRINT A CHAR A TO Z;
+
+    // for(char ch='A';ch<='Z';ch++){
+    //     cout<<ch<<endl;
+    // }
+
+    // 3. PRINT CHAR FROM Z TO A
+
+    // for(char ch='Z' ;ch>='A';ch--){
+    //     cout<<ch<<endl;
+    // }
+
+
+    // 4. PRINT NUMBER OF AP START WITH 220 AND END WITH 730 DIFFERENCE 7;
+
+    //  int val = 220;
+    //  int dif = 7;
+    //  int last = 730;
+
+    //  for(int i=val;i<=last;i+=dif){
+    //     cout<<i<<endl;
+    //  }
+
+    // 5. SUM OF SQUARE OF N NATURAL NUMBER
+
+    // int n;
+    // cin>>n;
+    // int sum =0;
+    // for(int i=1;i<=n;i++){
+    //     sum += i*i;
+    // }
+    // cout<<sum;
+
+
+    // 6. SUM OF CUBE OF N NATURAL NUMBER
+
+    // int n;
+    // cin>>n;
+    // int sum =0;
+    // for(int i=1;i<=n;i++){
+    //     sum += i*i*i;
+    // }
+    // cout<<sum;
+
+
+    // 7. PRINT NTH FIBONACCI NUMBER
+
+    // int prev=0;
+    // int curr=1;
+    // int n;
+    // cin>>n;
+    // for(int i=2;i<=n;i++){
+    //     int temp = curr;
+    //     curr = prev + curr;
+    //     prev = temp;
+    // } 
+    // cout<<curr;
+
+}
