97f70d1ac5662b51ec64e7ff7fe92e5033d0ce68
Amanchahar1507 <103381910+Amanchahar1507@users.noreply.github.com>
2023-09-26 21:18:38 +0530

    Add files via upload

---
+++ Days9.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // 1. First Pattern:
+       
+    //  1
+    //  1 2
+    //  1 2 3
+    //  1 2 3 4
+    //  1 2 3 4 5 
+    //  1 2 3 4 5 6 
+    //  1 2 3 4 5 6
+    
+    // for(int i=1;i<=6;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 2. Second Pattern:
+   
+    // A
+    // A B
+    // A B C
+    // A B C D
+    // A B C D E
+
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<=i;j++){
+    //         char ch = 'A' + (j-1);
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 3.Third Pattern:
+     
+    //    10
+    //    10 11
+    //    10 11 12
+    //    10 11 12 13
+    //    10 11 12 13 14
+    //    10 11 12 13 14 15
+
+    // for(int i=1;i<=6;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<j+9<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 4. Fourth Pattern:
+
+    //   A B C D
+    //   A B C
+    //   A B
+    //   A
+
+    // for(int i=4;i>=1;i--){
+    //     for(int j=1;j<=i;j++){
+    //         char ch = 'A'+(j-1);
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+}
