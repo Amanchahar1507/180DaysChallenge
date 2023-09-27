47a1a5d317c2b7eb924fef93d9978d543fde2cca
Amanchahar1507 <103381910+Amanchahar1507@users.noreply.github.com>
2023-09-27 22:34:31 +0530

    Add files via upload

---
+++ Days10.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//     1.     1
+//          2 1
+//        3 2 1
+//    4 3 2 1
+// 5 4 3 2 1          
+
+
+    // int n;
+    //  cin>>n;
+
+    //  for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i+1;j++)
+    //         cout<<"  ";
+        
+    //     for(int j=i;j>=1;j--)
+    //         cout<<j<<" ";
+        
+    //     cout<<endl;
+    //  }
+
+    // 2.     A
+    //       B B
+    //     C C C
+    //   D D D D
+    // E E E E E
+
+    // int n;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=(n-i);j++)
+    //         cout<<"  ";
+
+    //     for(int j=1;j<=i;j++){
+    //         char ch = 'A'+(j-1);
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+//     3.     5
+//          5 4
+//        5 4 3
+//      5 4 3 2 
+//    5 4 3 2 1
+
+// int n;
+// cin>>n;
+// for(int i=n;i>=1;i--){
+//     for(int j=1;j<=i-1;j++){
+//         cout<<"  ";
+//     }
+//     for(int j=n;j>=i;j--){
+//         cout<<j<<" ";
+//     }
+//     cout<<endl;
+// }
+
+    // 4.      E
+    //       E D
+    //     E D C
+    //   E D C B
+    // E D C B A
+      
+    // int n;
+    // cin>>n;
+
+    // for(int i=n;i>=1;i--){
+    //     for(int j=1;j<=(i-1);j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int j=n;j>=i;j--){
+    //         char ch = 'A'+(j-1);
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+}
