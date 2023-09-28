131d585f772863154b96940944626529b313c52d
Amanchahar1507 <103381910+Amanchahar1507@users.noreply.github.com>
2023-09-28 21:35:56 +0530

    Add files via upload

---
+++ Days11.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // 1.       *
+    //        *  *
+    //      *  *  *
+    //    *  *  *  *
+    //  *  *  *  *  *     
+
+    
+    // int n;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=(n-i);j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=i;j>=1;j--){
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+//     2.      1
+//           1 2 3
+//        1 2 3 4 5
+//     1 2 3 4 5 6 7
+//  1 2 3 4 5 6 7 8 9
+
+    // int n;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=(n-i);j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int j=1;j<=(2*i-1);j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+//     3.     A
+//           A B A
+//        A B C B A
+//     A B C D C B A
+//  A B C D E D C B A
+
+    // int n;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=(n-i);j++){
+    //         cout<<"  ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         char ch = 'A'+(j-1);
+    //         cout<<ch<<" ";
+    //     }
+    //     for(int j=i-1;j>=1;j--){
+    //         char ch = 'A' +(j-1);
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 4.      *
+    //        *  *
+    //      *  *  *
+    //    *  *  *  *
+    //  *  *  *  *  *
+    //  *  *  *  *  *
+    //   *  *  *  *
+    //     *  *  *  
+    //       *  *  
+    //        *
+
+    // int n;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=(n-i);j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+    // for(int i=n;i>=1;i--){
+    //     for(int j=1;j<=(n-i);j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+
+}
