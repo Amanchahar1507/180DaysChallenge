2066c42a466f96f33e38c67a1a6e8bbadcddfa3a
Amanchahar1507 <103381910+Amanchahar1507@users.noreply.github.com>
2023-09-29 21:04:21 +0530

    Add files via upload

---
+++ Days12.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n;
+    // cin>>n;
+
+    // if(n%2==0 && n>=0){
+    //     cout<<"yes";
+    // }
+    // else{
+    //     cout<<"No";
+    // }
+
+    // int n;
+    // cin>>n;
+
+    // if(n>=13 && n<=19){
+    //     cout<<"teneger";
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+
+
+    // int a,b,c;
+    // cin>>a;
+    // cin>>b;
+    // cin>>c;
+
+    // if(a>=b && a>=c){
+    //     cout<<"yes";
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+
+    // int t;
+    // cin>>t;
+
+    // if(t>=70  && t<=90){
+    //     cout<<"yes";
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+}
