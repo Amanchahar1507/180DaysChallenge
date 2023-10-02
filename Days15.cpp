5993c78a377e7ca1b0d72a18c5c7d48fafd13ace
Amanchahar1507 <103381910+Amanchahar1507@users.noreply.github.com>
2023-10-02 19:31:48 +0530

    Add files via upload

---
+++ Days15.cpp
@@ -0,0 +1,151 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+//    1. print even.
+
+    // int n;
+    // cin>>n;
+
+    // int i=1;
+    // while(i<=n){
+    //     if(i%2==0){
+    //         cout<<i<<" ";
+    //     }
+    //     i++;
+    // }
+
+    // int i=1;
+    // do{
+    //     if(i%2==0){
+    //         cout<<i<<" ";
+    //     }
+    //     i++;
+    // }while(i<=n);
+
+
+    // 2. factorial of n number using while do-while
+
+    // int n;
+    // cin>>n;
+
+    // int i=2;
+    // int fact =1;
+    // while(i<=n){
+    //     fact = fact*i;
+    //     i++;
+    // }
+    // cout<<fact<<" ";
+
+    // int i=2;
+    // int fact =1;
+    // do{
+    //     fact = fact*i;
+    //     i++;
+    // }while(i<=n);
+    // cout<<fact<<" ";
+
+    // 3. NUMBER DIVISIBLE BY 3 AND 5
+
+    // int n;
+    // cin>>n;
+
+    // int num;
+    // int i=1;
+    // while(i<=n){
+    //     if(i%3==0 && i%5==0){
+    //         cout<<i<<" ";
+    //     }
+    //     i++;
+    // }
+
+    // int i=1;
+    // do{
+    //     if(i%3==0 && i%5==0){
+    //         cout<<i<<" ";
+    //     }
+    //     i++;
+    // }while(i<=n);
+
+    // 4. PRINT MONTH USING SWITCH
+
+    // int n;
+    // cin>>n;
+
+    // switch(n){
+    //     case 1:
+    //     cout<<"jan"<<" ";
+    //     break;
+
+    //     case 2:
+    //     cout<<"feb"<<" ";
+    //     break;
+
+    //     case 3:
+    //     cout<<"mar"<<" ";
+    //     break;
+    //     case 4:
+    //     cout<<"apr"<<" ";
+    //     break;
+    //     case 5:
+    //     cout<<"may"<<" ";
+    //     break;
+    //     case 6:
+    //     cout<<"june"<<" ";
+    //     break;
+    //     case 7:
+    //     cout<<"july"<<" ";
+    //     break;
+    //     case 8:
+    //     cout<<"aug"<<" ";
+    //     break;
+    //     case 9:
+    //     cout<<"sep"<<" ";
+    //     break;
+    //     case 10:
+    //     cout<<"oct"<<" ";
+    //     break;
+    //     case 11:
+    //     cout<<"nov"<<" ";
+    //     break;
+    //     case 12:
+    //     cout<<"dec"<<" ";
+    //     break;
+    //     default:
+    //     cout<<"enter valid number";
+    //     break;
+
+        
+    // }
+
+// 5. print All character
+
+    // char ch = 'A';
+    // while(ch<='Z'){
+    //     cout<<ch<<" ";
+    //     ch++;
+    // }
+
+    // char ch = 'a';
+    // while(ch<='z'){
+    //     cout<<ch<<" ";
+    //     ch++;
+    // }
+
+    //NUMBER IS PRIME OR NOT;
+
+    // int n;
+    // cin>>n;
+
+    // int i=2;
+    // while(i<n){
+    //     if(n%i==0){
+    //         cout<<"not a prime number"<<" ";
+    //         return 0;
+    //     }
+    //     i++;
+    // }
+    // cout<<"prime number";
+
+}
